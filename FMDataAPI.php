b66caa1a478c3c1e5f2f0ebcd99fabc63ebc4779
Masayuki Nii <msyk@msyk.net>
2017-04-29 23:57:05 +0900

    Implemented all features but they aren't tested well.

--- FMDataAPI.php
+++ FMDataAPI.php
@@ -11,7 +11,8 @@ class FMDataAPI
     private $layoutTable = array();
     private $provider = NULL;
 
-    public function __construct($solution, $user, $password, $host = NULL, $port = NULL, $protocol = NULL)
+    public function __construct(
+        $solution, $user, $password, $host = NULL, $port = NULL, $protocol = NULL)
     {
         $this->provider = new CommunicationProvider(
             $solution, $user, $password, $host, $port, $protocol);
@@ -45,8 +46,26 @@ class FMDataAPI
         $this->provider->isCertVaridating = $value;
     }
 
-    public function httpStatus()    {
+    public function httpStatus()
+    {
         return $this->provider->httpStatus;
+    }
+
+    public function errorCode()
+    {
+        return $this->provider->errorCode;
+    }
+
+    public function startCommunication($layout)
+    {
+        $this->provider->login($layout);
+        $this->provider->keepAuth = true;
+    }
+
+    public function endCommunication()
+    {
+        $this->provider->keepAuth = false;
+        $this->provider->logout();
     }
 }
 
@@ -59,6 +78,42 @@ class FileMakerLayout
     {
         $this->restAPI = $restAPI;
         $this->layout = $layout;
+    }
+
+    public function startCommunication()
+    {
+        $this->restAPI->login($this->layout);
+        $this->restAPI->keepAuth = true;
+    }
+
+    public function endCommunication()
+    {
+        $this->restAPI->keepAuth = false;
+        $this->restAPI->logout();
+    }
+
+    /*
+     * $portal should be:
+     * array('portal1', portal2') or
+     * array('portal1' => array('offset'=>1,'range'=>5), 'portal2' => null)
+     */
+    private function buildPortalParameters($param)
+    {
+        $request = array();
+        if (array_values($param) === $param) {
+            $request["portal"] = $param;
+        } else {
+            $request["portal"] = array_keys($param);
+            foreach ($param as $portalName => $options) {
+                if (!is_null($options) && $options['range']) {
+                    $request["range.{$portalName}"] = $options['range'];
+                }
+                if (!is_null($options) && $options['offset']) {
+                    $request["offset.{$portalName}"] = $options['offset'];
+                }
+            }
+        }
+        return $request;
     }
 
     public function query($condition = NULL, $sort = NULL, $offset = -1, $range = -1, $portal = null)
@@ -75,8 +130,9 @@ class FileMakerLayout
             $request["range"] = $range;
         }
         if (!is_null($portal)) {
-            $request["portal"] = $portal;
+            $request = array_merge($request, $this->buildPortalParameters($portal));
         }
+        $fmrel = null;
         if (!is_null($condition)) {
             $request["query"] = $condition;
             $this->restAPI->callRestAPI("find", $this->layout, true, "POST", $request);
@@ -85,25 +141,85 @@ class FileMakerLayout
         }
         $result = $this->restAPI->responseBody;
         $this->restAPI->httpStatus = $this->restAPI->getCurlInfo("http_code");
-        $fmrel = new FMRelation($result->data, $result->result, $result->errorCode);
+        $this->restAPI->errorCode = property_exists($result, 'errorCode') ? $result->errorCode : -1;
+        $this->restAPI->errorMessage = property_exists($result, 'errorMessage') ? $result->errorMessage : null;
+        if ($result &&
+            property_exists($result, 'data') &&
+            property_exists($result, 'result') &&
+            property_exists($result, 'errorCode')
+        ) {
+            $fmrel = new FMRelation($result->data, $result->result, $result->errorCode);
+        }
         $this->restAPI->logout();
         return $fmrel;
     }
 
-    public function getRecord($recordId)
+    public function getRecord($recordId, $portal = null)
     {
+        $request = array();
+        $this->restAPI->login($this->layout);
+        if (!is_null($portal)) {
+            $request = array_merge($request, $this->buildPortalParameters($portal));
+        }
+        $this->restAPI->callRestAPI("record", $this->layout, true, "GET", $request, $recordId);
+        $result = $this->restAPI->responseBody;
+        $this->restAPI->httpStatus = $this->restAPI->getCurlInfo("http_code");
+        $fmrel = null;
+        $this->restAPI->errorCode = property_exists($result, 'errorCode') ? $result->errorCode : -1;
+        $this->restAPI->errorMessage = property_exists($result, 'errorMessage') ? $result->errorMessage : null;
+        if ($result) {
+            $fmrel = new FMRelation($result->data, "OK", $result->errorCode);
+        }
+        $this->restAPI->logout();
+        return $fmrel;
     }
 
     public function create($data)
     {
+        $this->restAPI->login($this->layout);
+        $request = array("data" => $data);
+        $this->restAPI->callRestAPI("record", $this->layout, true, "POST", $request);
+        $result = $this->restAPI->responseBody;
+        $this->restAPI->httpStatus = $this->restAPI->getCurlInfo("http_code");
+        $this->restAPI->errorCode = property_exists($result, 'errorCode') ? $result->errorCode : -1;
+        $this->restAPI->errorMessage = property_exists($result, 'errorMessage') ? $result->errorMessage : null;
+        $this->restAPI->logout();
+        return $result->recordId;
     }
 
-    public function delete($condition)
+    public function delete($recordId)
     {
+        $this->restAPI->login($this->layout);
+        $this->restAPI->callRestAPI("record", $this->layout, true, "DELETE", null, $recordId);
+        $result = $this->restAPI->responseBody;
+        $this->restAPI->httpStatus = $this->restAPI->getCurlInfo("http_code");
+        $this->restAPI->errorCode = property_exists($result, 'errorCode') ? $result->errorCode : -1;
+        $this->restAPI->errorMessage = property_exists($result, 'errorMessage') ? $result->errorMessage : null;
+        $this->restAPI->logout();
     }
 
-    public function update($condition, $data)
+    public function update($recordId, $data, $modId = -1)
     {
+        $this->restAPI->login($this->layout);
+        $request = array("data" => $data);
+        $this->restAPI->callRestAPI("record", $this->layout, true, "PUT", $request, $recordId);
+        $result = $this->restAPI->responseBody;
+        $this->restAPI->httpStatus = $this->restAPI->getCurlInfo("http_code");
+        $this->restAPI->errorCode = property_exists($result, 'errorCode') ? $result->errorCode : -1;
+        $this->restAPI->errorMessage = property_exists($result, 'errorMessage') ? $result->errorMessage : null;
+        $this->restAPI->logout();
+    }
+
+    public function setGlobalField($fields)
+    {
+        $this->restAPI->login($this->layout);
+        $request = array("globalFields" => $fields);
+        $this->restAPI->callRestAPI("global", $this->layout, true, "PUT", $request);
+        $result = $this->restAPI->responseBody;
+        $this->restAPI->httpStatus = $this->restAPI->getCurlInfo("http_code");
+        $this->restAPI->errorCode = property_exists($result, 'errorCode') ? $result->errorCode : -1;
+        $this->restAPI->errorMessage = property_exists($result, 'errorMessage') ? $result->errorMessage : null;
+        $this->restAPI->logout();
     }
 }
 
@@ -193,12 +309,73 @@ class FMRelation implements Iterator
                     }
                     break;
                 case "PORTALRECORD":
+                    $convinedName = "{$this->portalName}::{$fieldName}";
                     if (isset($this->data->$fieldName)) {
                         $value = $this->data->$fieldName;
+                    } else if (isset($this->data->$convinedName)) {
+                        $value = $this->data->$convinedName;
                     }
                     break;
                 default:
             }
+        }
+        return $value;
+    }
+
+    public function getRecordId()
+    {
+        $value = null;
+        switch ($this->result) {
+            case "OK":
+                if (isset($this->data[$this->pointer])) {
+                    if (isset($this->data[$this->pointer]->recordId)
+                    ) {
+                        $value = $this->data[$this->pointer]->recordId;
+                    }
+                }
+                break;
+            case "PORTAL":
+                if (isset($this->data[$this->pointer]) &&
+                    isset($this->data[$this->pointer]->recordId)
+                ) {
+                    $value = $this->data[$this->pointer]->recordId;
+                }
+                break;
+            case "RECORD":
+            case "PORTALRECORD":
+                if (isset($this->data) && isset($this->data->recordId)) {
+                    $value = $this->data->recordId;
+                }
+                break;
+        }
+        return $value;
+    }
+
+    public function getModId()
+    {
+        $value = null;
+        switch ($this->result) {
+            case "OK":
+                if (isset($this->data[$this->pointer])) {
+                    if (isset($this->data[$this->pointer]->modId)
+                    ) {
+                        $value = $this->data[$this->pointer]->modId;
+                    }
+                }
+                break;
+            case "PORTAL":
+                if (isset($this->data[$this->pointer]) &&
+                    isset($this->data[$this->pointer]->modId)
+                ) {
+                    $value = $this->data[$this->pointer]->modId;
+                }
+                break;
+            case "RECORD":
+            case "PORTALRECORD":
+                if (isset($this->data) && isset($this->data->modId)) {
+                    $value = $this->data->recordId;
+                }
+                break;
         }
         return $value;
     }
@@ -288,6 +465,9 @@ class CommunicationProvider
     private $responseHeader;
     public $responseBody;
     public $httpStatus;
+    public $errorCode;
+    public $errorMessage;
+    public $keepAuth = false;
 
     public $isDebug;
     public $isCertVaridating;
@@ -308,14 +488,18 @@ class CommunicationProvider
         }
     }
 
-    public function getURL($action, $layout)
+    public function getURL($action, $layout, $recordId = null)
     {
         return "{$this->protocol}://{$this->host}:{$this->port}" .
-            "/fmi/rest/api/{$action}/{$this->solution}/{$layout}";
+            "/fmi/rest/api/{$action}/{$this->solution}/{$layout}" .
+            ($recordId ? "/{$recordId}" : "");
     }
 
     public function login($layout)
     {
+        if ($this->keepAuth) {
+            return;
+        }
         $request = array(
             "user" => $this->user,
             "password" => $this->password,
@@ -332,34 +516,45 @@ class CommunicationProvider
 
     public function logout()
     {
+        if ($this->keepAuth) {
+            return;
+        }
         $this->callRestAPI("auth", "", true, "DELETE");
         $this->accessToken = NULL;
     }
 
-    public function callRestAPI($action, $layout, $isAddToken, $method = 'GET', $request = NULL, $recordId = -1)
+    public function callRestAPI($action, $layout, $isAddToken, $method = 'GET', $request = NULL, $recordId = null)
     {
-        $url = $this->getURL($action, $layout);
+        $methodLower = strtolower($method);
+        $url = $this->getURL($action, $layout, $recordId);
         $header = array();
-        if (!is_null($request))  {
+        if (!is_null($request) && $methodLower != 'get') {
             $header[] = "Content-Type: application/json";
         }
         if ($isAddToken) {
             $header[] = "FM-Data-token: {$this->accessToken}";
+        }
+        if ($methodLower == 'get' && !is_null($request)) {
+            $url .= '?';
+            foreach ($request as $key => $value) {
+                $url .= $key . '=' . (is_array($value) ? json_encode($value) : $value);
+            }
         }
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_VERBOSE, 0);
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_SSLVERSION, CURL_SSLVERSION_DEFAULT);
-        if (strtolower($method) == 'post') {
+        if ($methodLower == 'post') {
             curl_setopt($ch, CURLOPT_POST, 1);
-        } else if (strtolower($method) == 'put') {
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-        } else if (strtolower($method) == 'delete') {
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-        } else {
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        }
+        } else
+            if ($methodLower == 'put') {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+            } else if ($methodLower == 'delete') {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+            } else {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+            }
         if ($this->isCertVaridating) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
@@ -369,7 +564,9 @@ class CommunicationProvider
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
+        if ($methodLower != 'get') {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
+        }
         $response = curl_exec($ch);
         $this->curlInfo = curl_getinfo($ch);
         $this->curlErrorNumber = curl_errno($ch);
@@ -381,7 +578,7 @@ class CommunicationProvider
         $this->method = $method;
         $this->url = $url;
         $this->requestHeader = $header;
-        $this->requestBody = $request;
+        $this->requestBody = ($methodLower != 'get') ? $request : null;
         $this->responseHeader = substr($response, 0, $this->curlInfo["header_size"]);
         $this->responseBody = json_decode(substr($response, $this->curlInfo["header_size"]));
 
@@ -390,13 +587,16 @@ class CommunicationProvider
         }
     }
 
-    public function getCurlInfo($key)   {
+    public
+    function getCurlInfo($key)
+    {
         return $this->curlInfo[$key];
     }
 
-    public function debugOutput($isReturnValue = false)
+    public
+    function debugOutput($isReturnValue = false)
     {
-        $str  = "<div style='background-color: #DDDDDD'>URL: ";
+        $str = "<div style='background-color: #DDDDDD'>URL: ";
         $str .= $this->method . ' ' . htmlspecialchars($this->url);
         $str .= "</div>Added Request Header:<br><pre>";
         $str .= htmlspecialchars(var_export($this->requestHeader, true));
--- FMDataAPI_Sample.php
+++ FMDataAPI_Sample.php
@@ -25,19 +25,54 @@ foreach ($result as $record) {
     echo "<hr>";
 }
 $result->rewind();
-for ($i = 0 ; $i < $result->count(); $i++)    {
+for ($i = 0; $i < $result->count(); $i++) {
     echo "id: {$result->id},";
     echo "name: {$result->name},";
     echo "mail: {$result->mail}<hr>";
     $contacts = $result->contact_to;
-    for ($j = 0 ; $j < $contacts->count(); $j++)    {
+    for ($j = 0; $j < $contacts->count(); $j++) {
         echo "[PORTAL(contact_to)] id: {$contacts->field("id", "contact_to")},";
-        echo "summary: {$contacts->field("summary", "contact_to")}<hr>";
+        echo "summary: {$contacts->summary}<hr>";
         $contacts->next();
     }
     $result->next();
 }
-//$fm->postalcode->getRecord($recordId);
-//$fm->postalcode->create($data);
-//$fm->postalcode->delete($condition);
-//$fm->postalcode->update($condition, $data);
+$recId = $fmdb->postalcode->create(array("f3" => "field 3 data", "f7" => "field 7 data"));
+$result = $fmdb->postalcode->getRecord($recId);
+foreach ($result as $record) {
+    echo "f3: {$record->f3},";
+    echo "f4: {$record->f4},";
+    echo "f5: {$record->f5}<hr>";
+    echo "<hr>";
+}
+$fmdb->postalcode->update($recId, array("f3" => "field 3 modifed", "f8" => "field 8 update"));
+$result = $fmdb->postalcode->getRecord($recId);
+foreach ($result as $record) {
+    echo "f3: {$record->f3},";
+    echo "f4: {$record->f4},";
+    echo "f5: {$record->f5}<hr>";
+    echo "<hr>";
+}
+//$fmdb->postalcode->delete($recId);
+
+$recIds = array();
+$fmdb->postalcode->startCommunication();
+$recIds[] = $fmdb->postalcode->create(array("f3" => "field 3 data 1", "f7" => "field 7 data"));
+$recIds[] = $fmdb->postalcode->create(array("f3" => "field 3 data 2", "f7" => "field 7 data"));
+$recIds[] = $fmdb->postalcode->create(array("f3" => "field 3 data 3", "f7" => "field 7 data"));
+$recIds[] = $fmdb->postalcode->create(array("f3" => "field 3 data 4", "f7" => "field 7 data"));
+$recIds[] = $fmdb->postalcode->create(array("f3" => "field 3 data 5", "f7" => "field 7 data"));
+$recIds[] = $fmdb->postalcode->create(array("f3" => "field 3 data 6", "f7" => "field 7 data"));
+$recIds[] = $fmdb->postalcode->create(array("f3" => "field 3 data 7", "f7" => "field 7 data"));
+$fmdb->postalcode->endCommunication();
+var_export($recIds);
+echo "<hr>";
+
+$portal = array("contact_to");
+$result = $fmdb->person_layout->query(array(array("id" => "1")), null, 0, 1000000, null);
+foreach ($result as $record) {
+    $recordId = $record->getRecordId();
+    $partialResult = $fmdb->person_layout->getRecord($recordId, $portal);
+    var_export($partialResult);
+    echo "<hr>";
+}
