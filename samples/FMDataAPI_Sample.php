4b031e628b0e599bff775336ea9a148150115456
Masayuki Nii <nii@msyk.net>
2022-12-25 19:03:56 +0900

    Checking some methods (as like getFirstRecord) to work for all methods.

--- samples/FMDataAPI_Sample.php
+++ samples/FMDataAPI_Sample.php
@@ -19,7 +19,7 @@ try {
     // Instantiate the class FMDataAPI with database name, user name, password and host.
     // Although the port number and protocol can be set in parameters of constructor,
     // these parameters can be omitted with default values.
-    $fmdb = new FMDataAPI("TestDB", "web", "password", "localhost");
+    $fmdb = new FMDataAPI("TestDB", "web", "password", "192.168.17.130" /*"localhost"*/);
 
     //==============================
     //$fmdb = new FMDataAPI("TestDB", "web", "password", "localserver");
@@ -59,6 +59,7 @@ try {
     $pInfo = var_export($result, true);
     echo htmlspecialchars("Layout Metadata (Old): {$pInfo}", ENT_QUOTES, "UTF-8") . "<hr>";
 
+    exit;
     // The FMDataAPI has the property as the same name of layout. This sample database has the 'person_layout' layout,
     // so '$fmdb->person_layout' refers FMLayout object fo the proxy of the layout. FMLayout class has the 'query' method
     // and returns FileMakerRelation class's object. The condition spefied in parameter is same as FileMaker's Find Record API.
@@ -198,6 +199,7 @@ try {
 
     // A new record is created in "testtable" table.
     $recId = $fmdb->testtable->create();
+    echo "RecId = {$recId}";
     // The "testtable" table has a container filed "vc1". One image file is going to be uploaded to it.
     // The file path, record id and field name are required.
     $fmdb->testtable->uploadFile("cat.jpg", $recId, "vc1");
@@ -268,6 +270,14 @@ try {
             echo "<hr>";
         }
     }
+
+    //
+    $result = $fmdb->person_layout->query();
+    $first = $result->getFirstRecord();
+    echo "id field of the first record: {$first->field('id')}";
+    $contacts = $first->Contact;
+    echo "[PORTAL(contact_to)] id: {$contacts->field("id", "contact_to")}";
+
 } catch (Exception $e) {
     echo '<div><h3>例外発生</h3>', htmlspecialchars($e->getMessage(), ENT_QUOTES, "UTF-8"), "<div>";
 }
