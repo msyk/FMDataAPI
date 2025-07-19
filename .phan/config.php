fa9810b465a3e8a684b553f2b04d785c8e56d940
Atsushi Matsuo <famlog@gmail.com>
2025-07-19 10:14:45 +0900

    Run Phan on GitHub Actions

---
+++ .phan/config.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'target_php_version' => '8.4',
+
+    'directory_list' => [
+        'src',
+    ],
+
+    'exclude_analysis_directory_list' => [
+        'vendor/'
+    ],
+];
