c302bf7e9f4c956d8418b68bb613ac0b12396edd
Masayuki Nii <nii@msyk.net>
2026-04-22 22:59:36 +0900

    Revert "Add optional persistent sessions across PHP requests"

--- src/Supporting/CommunicationProvider.php
+++ src/Supporting/CommunicationProvider.php
@@ -148,13 +148,6 @@ class CommunicationProvider
      * @ignore
      */
     public bool $keepAuth = false;
-    /**
-     * @var bool Use persistent session for communication with FileMaker server. A persistent session is when a
-     * communication with FileMaker server is kept open for multiple PHP requests, making all PHP requests share the
-     * same session token.
-     * @ignore
-     */
-    public bool $keepPersistentSession = false;
 
     /**
      * @var bool
@@ -521,7 +514,6 @@ class CommunicationProvider
                 return true;
             }
         } catch (Exception $e) {
-            $this->storeToProperties();
             $this->accessToken = null;
             throw $e;
         }
@@ -536,7 +528,7 @@ class CommunicationProvider
      */
     public function logout(): void
     {
-        if ($this->keepAuth || $this->keepPersistentSession) {
+        if ($this->keepAuth) {
             return;
         }
         $params = ["sessions" => $this->accessToken];
